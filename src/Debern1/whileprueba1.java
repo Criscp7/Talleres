a2d37a1b522503fd80b32be7993fa003c5b5c86d
Cristian Pozo <Cristian Pozo@DESKTOP-CP0BFFG>
2021-07-30 17:43:10 -0500

    while

---
+++ src/Debern1/whileprueba1.java
@@ -0,0 +1,30 @@
+package Debern1;
+import java.util.Scanner;
+
+public class whileprueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner lector =new Scanner (System.in);
+int n=0;
+float suma=0;
+float elementos=0;
+float promedio=0;
+System.out.print("Ingrese un numero positivo o 0 para salir:");
+n=lector.nextInt();
+suma=suma+n;
+while(n!=0) {
+	System.out.print("Ingrese un numero positivo:");
+	n=lector.nextInt();
+	
+	suma=suma+n;
+	elementos++;
+	promedio =suma/elementos;
+	
+}
+System.out.println("Suma total:"+suma);
+System.out.println("numero de numeros ingresados:"+elementos);
+System.out.println("promedio total:"+promedio);
+	}
+
+}
