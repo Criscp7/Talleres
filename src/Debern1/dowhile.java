5af1c2fe685da13004c80b95d02c63fc8a2a8a83
Cristian Pozo <Cristian Pozo@DESKTOP-CP0BFFG>
2021-07-30 17:54:01 -0500

    ya

---
+++ src/Debern1/dowhile.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Debern1;
+
+/**
+ * @author Cristian Pozo
+ *
+ */
+public class dowhile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
