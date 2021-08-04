bf4dd8b98cbff9c7aa90ae86c0032fc50c867669
Cristian Pozo <Cristian Pozo@DESKTOP-CP0BFFG>
2021-08-04 14:48:08 -0500

    de

---
+++ src/ejercicios/matrices/Mainmatrices.java
@@ -0,0 +1,23 @@
+package ejercicios.matrices;
+
+public class Mainmatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//1.Declarar una matriz A de tipo char con 5 filas y 4 columnas
+		char A [][]=new char [5][4];
+		
+		//2. Declarar una matriz B de tipo char con 5 filas y 6 columnas 
+		char B [][]=new char [5][6];
+		//3. Declarar una matriz miMatriz de long con 2 filas y 4 columnas 
+		long miMatriz[][]= new long [2][3];
+		//4. Declara una matriz miMatriz1 de long con 1 fila y 2 columnas
+		// y asignar a la posicion [0][0] el valor de 2, posicion [1][1] el valor de 8
+		long miMatriz1[][]= new long [1][2];
+		miMatriz1[0][0]=2;
+		miMatriz1[1][1]=8;
+		miMatriz1[2][2]=2;
+		
+	}
+
+}
--- src/ejercicios/varios/vectores/matrices/MainVector2.java
+++ src/ejercicios/varios/vectores/matrices/MainVector2.java
@@ -1,9 +1,23 @@
 package ejercicios.varios.vectores.matrices;
 
+import java.util.Scanner;
+
 public class MainVector2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Declare un vector X cuya longitud se solicite al usuario, de tipo int y construya un programa que pida al
+		//usuario que 
+		//ingrese los valores para cada posicion
+		// y finalmente que se imprima todos los valores ingresados
+     int longitud = ingrese por teclado; 
+		int X[] = new int [longitud];
+		
+		for (int i = 0;i<3;i++) {
+			System.out.println("Ingrese un valor de la posicion: "+i);
+			Scanner lector=new Scanner(System.in);
+			int n=(int)lector.next().charAt(0);
+			X[i]=n; 
 
 	}
 
